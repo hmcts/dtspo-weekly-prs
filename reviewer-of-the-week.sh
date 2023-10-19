5990df612ac5ec93c164b3a944dba8e9a15d47d5
Jordan Hoey <jordan.hoey@justice.gov.uk>
2023-10-19 14:59:58 +0100

    Testing

---
+++ reviewer-of-the-week.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+token=$1
+
+gh auth login --with-token ${token}
+
+ghusers=('JordanHoey96')
+
+for ghuser in ${ghusers[@]}
+do
+    echo $ghuser
+    last100prs=$(gh search prs --reviewed-by $ghuser --json closedAt,url,title --sort updated --limit 10)
+    7DaysAgo=$(date +%F -d '7 days ago')
+    while read pr; do
+        closedDateString=$(jq -r '.closedAt' <<< $pr)
+        title=$(jq -r '.title' <<< $pr)
+        url=$(jq -r '.url' <<< $pr)
+        closedDate=$(date -d $closedDateString)
+        if [[ $closedDate -le $7DaysAgo ]] ; then
+            echo "$title pr was closed on $closedDate URL: $url"
+        fi
+    done < <(jq -c '.[]' <<<$last100prs)
+done
