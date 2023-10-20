48c4e4cc9a4740d8fbf07b9ea533f146b17afedb
Jordan Hoey <jordan.hoey@justice.gov.uk>
2023-10-20 09:43:56 +0100

    testing

--- scripts/get-prs-by-user.sh
+++ scripts/get-prs-by-user.sh
@@ -5,15 +5,15 @@ gh auth login --with-token ${token}
 repos=$(gh search prs  --owner hmcts --author app/renovate --state=open  --sort=created --json repository -L 300 | jq -r '. | unique_by(.repository.name)' | jq -r '.[].repository.name')
 respo+=($(gh search prs "[updatecli]" --owner hmcts  --state=open  --sort=created --json repository -L 300 | jq -r '. | unique_by(.repository.name)' | jq -r '.[].repository.name'))
 
-ghusers=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /orgs/HMCTS/teams/platops-blue/members | jq -r '.[].login')
-#ghusers=('endakelly' 'JusticeCarl' 'louisehuyton' 'Tyler-35' 'cpareek' 'JordanHoey96')
+#ghusers=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /orgs/HMCTS/teams/platops-blue/members | jq -r '.[].login')
+ghusers=('JordanHoey96')
 
 for ghuser in ${ghusers[@]}
 do
     echo $ghuser
     for repo in  ${repos[@]}
     do  
-       gh pr list --author app/renovate  --state open --repo hmcts/${repo}  --json reviewRequests,url | jq -r --arg ghuser "$ghuser" '.[] |select(.reviewRequests[].login==$ghuser) | .url '
-       gh pr list --search "[updatecli]" --state open --repo hmcts/${repo}  --json reviewRequests,url | jq -r --arg ghuser "$ghuser" '.[] |select(.reviewRequests[].login==$ghuser) | .url '
+       gh pr list --author app/renovate  --state open --repo hmcts/${repo}  --json reviewRequests,url | jq -r --arg ghuser "$ghuser" '.[] |select(.reviewRequests[].login==$ghuser) | .url | .title'
+       gh pr list --search "[updatecli]" --state open --repo hmcts/${repo}  --json reviewRequests,url | jq -r --arg ghuser "$ghuser" '.[] |select(.reviewRequests[].login==$ghuser) | .url | .title'
     done 
 done
