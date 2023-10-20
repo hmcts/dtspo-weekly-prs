e47e6c0b989f366d5730d8ed2a0afb70c3ee2f50
Jordan Hoey <jordan.hoey@justice.gov.uk>
2023-10-20 09:07:19 +0100

    testing

---
+++ get-prs-by-userv2.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+token=$1
+
+gh auth login --with-token ${token}
+
+#ghusers=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /orgs/HMCTS/teams/platops-blue/members | jq -r '.[].login')
+ghusers=('JordanHoey96')
+
+for ghuser in ${ghusers[@]}
+do
+    echo $ghuser
+    gh search prs  --owner hmcts --author app/renovate --state=open  --sort=created --review-requested $ghuser 
+done
