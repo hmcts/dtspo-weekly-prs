380c314c97d4b36b0ec16fd50afe1d7a68da120b
Jordan Hoey <jordan.hoey@justice.gov.uk>
2024-02-12 15:41:58 +0000

    testing

--- scripts/send-slack-message.sh
+++ scripts/send-slack-message.sh
@@ -6,4 +6,4 @@ CHANNEL_NAME=$2
 MESSAGE=$(cat test.json)
 #curl -s -X POST --data-urlencode "payload={\"channel\": \"${CHANNEL_NAME}\", \"username\": \"Plato\", \"text\": \"${MESSAGE}\", \"icon_emoji\": \":plato:\"}" ${WEBHOOK_URL}
 
-curl -X POST -H 'Content-type: application/json' --data ${MESSAGE} ${WEBHOOK_URL}
+curl -X POST -H 'Content-type: application/json' --data $MESSAGE ${WEBHOOK_URL}
